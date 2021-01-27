cc08d2202026a6b65d88bf492c70bd193da8e632
Miguel Rios <mrios0495@gmail.com>
2021-01-27 17:14:57 -0500

    Initial commit of Optimizer

---
+++ optimizer.py
@@ -0,0 +1,10 @@
+class Optimizer:
+    
+    def __init__(self, params, lr) -> None:
+        self.params, self.lr = list(params), lr
+
+    def step(self, *args, **kwargs):
+        for p in self.params: p.data -= p.grad.data * self.lr
+
+    def zero_grad(self, *args, **kwargs):
+        for p in self.params: p.grad = None
