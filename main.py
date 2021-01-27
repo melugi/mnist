a4fd998c2d235116603e8cc5fbdba58e927938b0
Miguel Rios <mrios0495@gmail.com>
2021-01-27 16:33:27 -0500

    Adds mnist_loss to main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import torch
+from torch.functional import Tensor
 from numpy import *
 import torchvision
 
+def mnist_loss(self, predictions, targets) -> Tensor:
+        predictions = torch.sigmoid(predictions)
+        return torch.where(targets==1, 1-predictions, predictions).mean()
+
 test_set = torchvision.datasets.MNIST('./', train=False, download=True, transform=None)
 train_set = torchvision.datasets.MNIST('./', train=True, download=True, transform=None)
-  
