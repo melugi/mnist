a3938a34d78107667e70c3e12c81fdf1f4870f93
Miguel Rios <mrios0495@gmail.com>
2021-01-27 19:11:11 -0500

    Implements validate and batch_accuracy

--- learner.py
+++ learner.py
@@ -1,42 +1,45 @@
 import torch
+from optimizer import Optimizer
 from torch.functional import Tensor
+from torch.utils.data.dataloader import DataLoader
 
 class Learner:
 
-    def __init__(self, data, model, optimizer, loss_func, metric_func) -> None:
+    def __init__(self,
+                data: DataLoader,
+                model,
+                optimizer: Optimizer,
+                loss_func,
+                metric_func
+                ) -> None:
         self.data = data
         self.model = model
         self.optimizer = optimizer
         self.loss_func = loss_func
         self.metric_func = metric_func
 
-    def fit(self, epochs, learning_rate) -> None:
+    def fit(self, epochs: int) -> None:
         for i in range(epochs):
-            self.train(learning_rate)
+            self.train()
             print(self.validate(), end=' ')
 
-    '''
-    TODO: Write train function to:
-        - Loop through data (Data contains X, Y tuples. X is the prediction, Y is the true result)
-        - Make predictions
-        - Calculate the loss
-        - Calculate gradient
-        - Use the gradient to step down on each parameter
-    '''
     def train(self) -> None:
         for x, y in self.data:
             self.calc_grad(x, y)
             self.optimizer.step()
             self.optimizer.zero_grad()
 
-    def calc_grad(self, x, y):
+    def calc_grad(self, x: Tensor, y: Tensor):
         prediction = self.model(x)
         loss = self.loss_func(prediction, y)
         loss.backward()
 
     def batch_accuracy(self, xb, yb) -> float:
-        pass
+        predictions = xb.sigmoid()
+        correct = (predictions > 0.5) == yb
+        return correct.float().mean()
 
     def validate(self) -> float:
-        pass
+        accs = [self.batch_accuracy(xb, yb) for xb, yb in self.data]
+        return round(torch.stack(accs).mean().item(), 4)
 
