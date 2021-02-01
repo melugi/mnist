e19b59eeb57ab913db25f8613e92903a8d9bfbcb
Miguel Rios <mrios0495@gmail.com>
2021-02-01 15:36:52 -0500

    Fixes small naming mistakes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from torch.functional import Tensor
 from numpy import *
 import torchvision
 
-def initalize_params(size, std=1.0) -> Tensor: return (torch.rand(size)*std).requires_grad()
+def initalize_params(size, std=1.0) -> Tensor: return (torch.rand(size)*std).requires_grad_()
 
 def mse(predictions, targets) -> Tensor: return ((predictions - targets)**2).mean().sqrt()
 
@@ -16,18 +16,15 @@ def batch_accuracy(xb, yb) -> float:
         correct = (predictions > 0.5) == yb
         return correct.float().mean()
 
-test_set = torch.utils.data.Dataloader(
+test_set = torch.utils.data.DataLoader(
     torchvision.datasets.MNIST('./', train=False, download=True, 
                                 transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor()])),
     batch_size=64, shuffle=True)
 
-train_set = torch.utils.data.Dataloader(
+train_set = torch.utils.data.DataLoader(
     torchvision.datasets.MNIST('./', train=True, download=True, 
                                 transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor()])),
     batch_size=500, shuffle=True)
 
-
 weights = initalize_params(28*28)
 bias = initalize_params(1)
-
-
